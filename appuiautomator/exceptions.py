3e9202c683bb36266818de14abb0cd1c67c62187
Kelvin.Ye <testmankelvin@163.com>
2020-04-02 00:40:57 +0800

    update

--- appuiautomator/exceptions.py
+++ appuiautomator/exceptions.py
@@ -8,33 +8,9 @@ from appuiautomator.utils.logger import get_logger
 log = get_logger(__name__)
 
 
-class BaseError(Exception):
+class PageError(Exception):
     pass
 
 
 class PageElementError(Exception):
     pass
-
-
-class U2ClientError(BaseError):
-    """Uiautomator2 Python客户端异常类
-    """
-
-    def __init__(self, msg=None) -> None:
-        super().__init__(self)
-        self.msg = msg
-
-    def __str__(self):
-        return self.msg
-
-
-class ServiceError(BaseError):
-    """业务异常类
-    """
-
-    def __init__(self, msg=None) -> None:
-        super().__init__(self)
-        self.msg = msg
-
-    def __str__(self):
-        return self.msg
