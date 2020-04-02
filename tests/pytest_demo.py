e61bfaad4d1deecf0d5ff8fa106e9d0102650a09
Kelvin.Ye <testmankelvin@163.com>
2020-04-02 16:57:00 +0800

    update

--- tests/pytest_demo.py
+++ tests/pytest_demo.py
@@ -5,6 +5,7 @@
 # @Author  : Kelvin.Ye
 import os
 
+import allure
 import pytest
 
 
@@ -48,8 +49,59 @@ def test_parametrize(key_name):
     print(key_name)
 
 
+@allure.feature('标注功能or模块')
+class TestAllureDemo:
+    @allure.story("标注子功能or模块")
+    def test_story(self):
+        pass
+
+    @allure.severity('标注案例重要程度')
+    def test_severity(self):
+        pass
+
+    @allure.step('标注案例步骤')
+    def test_step(self):
+        pass
+
+    def test_step2(self):
+        with allure.step('步骤1'):
+            a = 1
+        with allure.step('步骤2'):
+            b = 1
+        assert a == b
+
+    def test_attach_info(self):
+        allure.attach('附加信息')
+
+    def test_attach_picture(self):
+        with open(r'filepath.jpg', 'rb') as file:
+            allure.attach(file.read(), name='附加图片', attachment_type=allure.attachment_type.JPG)
+
+    @allure.testcase('案例链接', '链接名称')
+    def test_testcase(self):
+        pass
+
+    @allure.issue('问题链接', '链接名称')
+    def test_issue(self):
+        pass
+
+    @allure.description('案例描述')
+    def test_description(self):
+        pass
+
+
 if __name__ == '__main__':
     """python代码中运行pytest
     """
     pytest.main(['-s', f'{os.path.basename(__file__)}::test_parametrize'])
-    pytest.main(['-s', f'{os.path.basename(__file__)}', '--html=report.html'])
+    pytest.main(['-s', f'{os.path.basename(__file__)}', '--html=./report.html'])
+    pytest.main(['-s', f'{os.path.basename(__file__)}', '--alluredir=./report', '--clean-alluredir'])
+
+"""
+生成 Allure HTML报告
+
+下载 allure-command，下载地址: https://github.com/allure-framework/allure2/releases
+解压后，PATH环境变量添加 allure/bin路径
+
+allure generate report -o html --clean
+"""
