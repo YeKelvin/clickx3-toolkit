c9c3bcf777ac1d4f017f498635a6e09c8d273573
Kelvin.Ye <testmankelvin@163.com>
2020-04-03 19:21:14 +0800

    update

---
+++ appuiautomator/wda/app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File    : app
+# @Time    : 2020/4/3 19:14
+# @Author  : Kelvin.Ye
+
---
+++ appuiautomator/wda/device.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File    : device
+# @Time    : 2020/4/3 19:14
+# @Author  : Kelvin.Ye
+
