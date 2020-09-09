6f5fbc655c128560dddba6b18a144cfb57563283
Kelvin.Ye <testmankelvin@163.com>
2020-09-09 16:02:30 +0800

    update

--- appuiautomator/utils/rsa_util.py
+++ appuiautomator/utils/rsa_util.py
@@ -3,3 +3,44 @@
 # @File    : rsa_util
 # @Time    : 2020/9/8 20:49
 # @Author  : Kelvin.Ye
+import base64
+
+from Crypto import Random
+from Crypto.Cipher import PKCS1_v1_5
+from Crypto.PublicKey import RSA
+from Crypto.PublicKey.RSA import RsaKey
+
+
+def generate_rsa_key() -> (bytes, bytes):
+    """生产RSA公钥和私钥
+    """
+    rsa = RSA.generate(2048, Random.new().read)
+    public_key = rsa.publickey().exportKey()
+    private_key = rsa.exportKey()
+    return public_key, private_key
+
+
+def encrypt_by_rsa_public_key(content, public_key):
+    """通过RSA公钥加密
+
+    :param content:     加密内容
+    :param public_key:  RSA公钥
+    :return:            密文
+    """
+    rsakey = RSA.importKey(public_key)
+    cipher = PKCS1_v1_5.new(rsakey)
+    ciphertext = base64.b64encode(cipher.encrypt(content))
+    return ciphertext
+
+
+def decrypt_by_rsa_private_key(ciphertext, private_key):
+    """通过RSA私钥解密
+
+    :param ciphertext:  密文
+    :param private_key: RSA私钥
+    :return:            明文
+    """
+    rsakey = RSA.importKey(private_key)
+    cipher = PKCS1_v1_5.new(rsakey)
+    plaintext = cipher.decrypt(base64.b64decode(ciphertext), None)
+    return plaintext.decode('utf8')
