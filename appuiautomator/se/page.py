4ff0bd0db95e032fef9337f158497e2a25576456
Kelvin.Ye <testmankelvin@163.com>
2020-10-13 19:06:42 +0800

    update

--- appuiautomator/se/page.py
+++ appuiautomator/se/page.py
@@ -11,9 +11,22 @@ log = get_logger(__name__)
 class Page:
     url = None
 
-    def __init__(self, driver):
-        self.driver = driver
+    @property
+    def driver(self):
+        return self._driver
+
+    @driver.setter
+    def driver(self, value):
+        self._driver = value
+        if self.driver:
+            for attr in self.__dict__.values():
+                if isinstance(attr, self.__class__):
+                    attr.device = value
+
+    def __init__(self, driver=None):
+        if driver:
+            self._driver = driver
 
     def get(self):
         if self.url:
-            self.driver.get(self.hostname + self.uri)
+            self.driver.get(self.hostname + self.url)
