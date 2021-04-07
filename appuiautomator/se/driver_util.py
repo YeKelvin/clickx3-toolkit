78a2bd9db19838efcaa19ec5877c3742fd4c9584
Kelvin.Ye <testmankelvin@163.com>
2021-04-07 09:28:34 +0800

    🎈perf: 增加容错逻辑

--- appuiautomator/se/driver_util.py
+++ appuiautomator/se/driver_util.py
@@ -8,24 +8,40 @@ import os
 from appuiautomator.utils.config import resources_path
 
 
-def last_version_dir(driver_name):
+def last_version(driver_name):
+    """根据driver-name获取./resources/webdrive/目录下最新版本driver的绝对路径
+
+    Args:
+        driver_name (str): chrome | firefox
+
+    Returns:
+        None | str
+    """
     driver_dir = os.path.join(resources_path(), 'webdrive', driver_name)
+    if not os.path.exists(driver_dir):
+        return None
     verisons = os.listdir(driver_dir)
     verisons.sort()
     return os.path.join(driver_dir, verisons[-1])
 
 
 def chromedriver_last_version_path():
-    return os.path.join(last_version_dir('chrome'), 'chromedriver.exe')
+    return os.path.join(last_version('chrome'), 'chromedriver.exe')
 
 
 def chromedriver_log_path():
-    return os.path.join(last_version_dir('chrome'), 'chromedriver.log')
+    log_dir = os.path.join(resources_path(), 'webdrive', 'chrome')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    return os.path.join(log_dir, 'chromedriver.log')
 
 
 def gecodriver_last_version_path():
-    return os.path.join(last_version_dir('firefox'), 'geckodriver.exe')
+    return os.path.join(last_version('firefox'), 'geckodriver.exe')
 
 
 def gecodriver_log_path():
-    return os.path.join(last_version_dir('firefox'), 'geckodriver.log')
+    log_dir = os.path.join(resources_path(), 'webdrive', 'firefox')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+    return os.path.join(log_dir, 'geckodriver.log')
