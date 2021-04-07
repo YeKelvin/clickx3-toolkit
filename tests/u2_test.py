807172bed29f5e0653172ba0c3555fbfcb9ed326
Kelvin.Ye <testmankelvin@163.com>
2021-04-07 12:51:57 +0800

    🧪test: new test

--- tests/u2_test.py
+++ tests/u2_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @File    : u2_test
+# @File    : u2_test.py
 # @Time    : 2020/4/2 16:14
 # @Author  : Kelvin.Ye
 import uiautomator2 as u2
--- tests/wda_test.py
+++ tests/wda_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @File    : wda_test
+# @File    : wda_test.py
 # @Time    : 2020/4/2 16:14
 # @Author  : Kelvin.Ye
 import wda
@@ -9,3 +9,7 @@ import wda
 def test_wda():
     s = wda.Client().session()
     print(s)
+
+
+if __name__ == '__main__':
+    ...
