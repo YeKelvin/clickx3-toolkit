f2c45846743d0f7b6951c5770681bb4c6c2d3c06
Kelvin.Ye <testmankelvin@163.com>
2021-04-08 21:50:47 +0800

    ✨feat: add sleep method

--- appuiautomator/u2/device.py
+++ appuiautomator/u2/device.py
@@ -42,14 +42,17 @@ class Device(u2.Device):
         # 直接把u2.Device的属性字典复制过来
         self.__dict__ = u2d.__dict__
 
-    def run_adb_shell(self, command: str):
+    def sleep(self, secs: float = 1):
+        sleep(secs)
+
+    def adb_shell(self, command: str):
         log.info(f'执行adb命令:[ {command} ]')
         output, exit_code = self.shell(command)
         log.info(f'adb shell output:\n {output}')
         log.info(f'adb shell exitCode:[ {exit_code} ]')
 
     def open_url(self, url: str):
-        self.run_adb_shell(f'am start -a android.intent.action.VIEW -d "{url}"')
+        self.adb_shell(f'am start -a android.intent.action.VIEW -d "{url}"')
 
     def app_restart(self, package_name, activity=None):
         log.info(f'重启app:[ {package_name} ]')
@@ -77,7 +80,7 @@ class Device(u2.Device):
     def adb_input(self, input_content: str) -> None:
         """通过adb shell input text 进行内容输入，不限于字母、数字、汉字等
         """
-        self.run_adb_shell(f'input text {input_content}')
+        self.adb_shell(f'input text {input_content}')
 
     def adb_refresh_gallery(self, file_uri: str) -> None:
         """上传图片至系统图库后，要手动广播通知系统图库刷新
@@ -85,7 +88,7 @@ class Device(u2.Device):
         log.info('ADB广播刷新系统图库')
         if file_uri.startswith('/'):
             file_uri = file_uri[1:]
-        self.run_adb_shell(
+        self.adb_shell(
             fr'am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d file:///{file_uri}')
 
     def adb_screencap_local(self, path: str = None) -> None:
@@ -97,7 +100,7 @@ class Device(u2.Device):
             path = f'/sdcard/DCIM/Screenshots/{filename}'
 
         command = f'screencap -p {path}'
-        self.run_adb_shell(command)
+        self.adb_shell(command)
 
     def get_toast_message(self, wait_timeout=10, cache_timeout=10, default=None):
         log.info('获取toast信息')
