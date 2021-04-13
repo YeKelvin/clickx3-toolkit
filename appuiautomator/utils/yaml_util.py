cc7fb9dce6990980472f45a1ec3c49ce9ddbad62
Kelvin.Ye <testmankelvin@163.com>
2021-04-13 18:12:11 +0800

    ✨feat: add load_testdata method

--- appuiautomator/utils/yaml_util.py
+++ appuiautomator/utils/yaml_util.py
@@ -3,7 +3,10 @@
 # @File    : time_util.py
 # @Time    : 2021/4/10 21:39
 # @Author  : Kelvin.Ye
+import os
+
 import yaml
+from appuiautomator.utils.config import resources_path
 
 
 def load(stream):
@@ -19,3 +22,11 @@ def load(stream):
 def dump(data):
     """序列化"""
     return yaml.safe_dump(data, encoding='utf-8')
+
+
+# TODO: 加结果缓存
+def load_testdata(name):
+    if not name.endswith('.yaml'):
+        name = name + '.yaml'
+    file_path = os.path.join(resources_path(), 'testdata', name)
+    return load(file_path)
