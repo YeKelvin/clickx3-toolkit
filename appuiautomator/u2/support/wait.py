2ee001e6e9f7b95c2c1d3a59727ede8065bf10f0
Kelvin.Ye <testmankelvin@163.com>
2021-04-16 13:47:25 +0800

    ✨feat: 参考selenium wait逻辑实现自定义的wait方法

--- appuiautomator/u2/support/wait.py
+++ appuiautomator/u2/support/wait.py
@@ -7,32 +7,22 @@
 # Not yet implemented
 
 import time
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
 
-POLL_FREQUENCY = 0.5  # How long to sleep inbetween calls to the method
-IGNORED_EXCEPTIONS = (NoSuchElementException,)  # exceptions ignored during calls to the method
+from appuiautomator.utils.log_util import get_logger
+from uiautomator2.exceptions import TimeoutException, UiObjectNotFoundError, XPathElementNotFoundError
+
+log = get_logger(__name__)
+
+
+# 轮询频率（间隔等待时间）
+POLL_FREQUENCY = 0.5
+# 需要忽略的Exception列表
+IGNORED_EXCEPTIONS = (UiObjectNotFoundError, XPathElementNotFoundError)
 
 
 class U2DeviceWait(object):
-    def __init__(self, driver, timeout, poll_frequency=POLL_FREQUENCY, ignored_exceptions=None):
-        """Constructor, takes a WebDriver instance and timeout in seconds.
-
-           :Args:
-            - driver - Instance of WebDriver (Ie, Firefox, Chrome or Remote)
-            - timeout - Number of seconds before timing out
-            - poll_frequency - sleep interval between calls
-              By default, it is 0.5 second.
-            - ignored_exceptions - iterable structure of exception classes ignored during calls.
-              By default, it contains NoSuchElementException only.
-
-           Example:
-            from selenium.webdriver.support.ui import WebDriverWait \n
-            element = WebDriverWait(driver, 10).until(lambda x: x.find_element_by_id("someId")) \n
-            is_disappeared = WebDriverWait(driver, 30, 1, (ElementNotVisibleException)).\ \n
-                        until_not(lambda x: x.find_element_by_id("someId").is_displayed())
-        """
-        self._driver = driver
+    def __init__(self, device, timeout, poll_frequency=POLL_FREQUENCY, ignored_exceptions=None):
+        self._device = device
         self._timeout = timeout
         self._poll = poll_frequency
         # avoid the divide by zero
@@ -46,14 +36,7 @@ class U2DeviceWait(object):
                 exceptions.append(ignored_exceptions)
         self._ignored_exceptions = tuple(exceptions)
 
-    def __repr__(self):
-        return '<{0.__module__}.{0.__name__} (session="{1}")>'.format(
-            type(self), self._driver.session_id)
-
     def until(self, method, message=''):
-        """Calls the method provided with the driver as an argument until the \
-        return value is not False."""
-        screen = None
         stacktrace = None
 
         end_time = time.time() + self._timeout
@@ -62,17 +45,15 @@ class U2DeviceWait(object):
                 value = method(self._driver)
                 if value:
                     return value
-            except self._ignored_exceptions as exc:
-                screen = getattr(exc, 'screen', None)
-                stacktrace = getattr(exc, 'stacktrace', None)
+            except self._ignored_exceptions as e:
+                stacktrace = getattr(e, 'stacktrace', None)
             time.sleep(self._poll)
             if time.time() > end_time:
                 break
-        raise TimeoutException(message, screen, stacktrace)
+        log.error(f'Stacktrace:\n{stacktrace}')
+        raise TimeoutException(message)
 
     def until_not(self, method, message=''):
-        """Calls the method provided with the driver as an argument until the \
-        return value is False."""
         end_time = time.time() + self._timeout
         while True:
             try:
