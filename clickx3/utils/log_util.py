ef9f1397e3118037c0b948f9ff7b98da1b86af93
Kelvin.Ye <testmankelvin@163.com>
2021-05-08 13:08:22 +0800

    ✨ feat: 增加logger配置，过滤部分冗余日志

--- clickx3/utils/log_util.py
+++ clickx3/utils/log_util.py
@@ -4,18 +4,18 @@
 # @Time    : 2019/8/27 13:48
 # @Author  : Kelvin.Ye
 import logging
+from logging.config import dictConfig
 
 from clickx3.utils import project
-
 
 # 日志格式
 LOG_FORMAT = '[%(asctime)s][%(levelname)s][%(name)s.%(funcName)s %(lineno)d] %(message)s'
 
 # 日志级别
-LEVEL = project.config.get('log', 'level')
+LOG_LEVEL = project.config.get('log', 'level')
 
 # 日志文件名称
-LOG_FILE_NAME = project.config.get('log', 'level')
+LOG_FILE_NAME = project.config.get('log', 'name')
 
 # 日志格式
 FORMATTER = logging.Formatter(LOG_FORMAT)
@@ -24,10 +24,39 @@ FORMATTER = logging.Formatter(LOG_FORMAT)
 CONSOLE_HANDLER = logging.StreamHandler()
 CONSOLE_HANDLER.setFormatter(FORMATTER)
 
+# logger配置
+dictConfig({
+    'version': 1,
+    'root': {
+        'propagate': False,
+        'level': LOG_LEVEL,  # handler的level会覆盖掉这里的level
+        'handlers': ['console']
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': LOG_FORMAT
+        }
+    },
+    'loggers': {
+        'urllib3.connectionpool': {
+            'level': 'ERROR'
+        },
+        'imageio_ffmpeg': {
+            'level': 'ERROR'
+        }
+    }
+})
+
 
 def get_logger(name) -> logging.Logger:
     logger = logging.getLogger(name)
     logger.propagate = False
-    logger.setLevel(LEVEL)
+    logger.setLevel(LOG_LEVEL)
     logger.addHandler(CONSOLE_HANDLER)
     return logger
