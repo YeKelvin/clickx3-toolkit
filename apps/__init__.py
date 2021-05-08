2805ce519b625796f8e58eb5227b43fcd3ebee94
Kelvin.Ye <testmankelvin@163.com>
2021-05-08 16:47:00 +0800

    ✨ feat: 删除apps模块，其实没啥用

--- apps/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File    : __init__.py
-# @Time    : 2021/5/8 16:35
-# @Author  : Kelvin.Ye
--- apps/chrome/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File    : __init__.py
-# @Time    : 2021/4/28 11:10
-# @Author  : Kelvin.Ye
